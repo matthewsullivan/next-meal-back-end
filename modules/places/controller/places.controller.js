f09d033d0a85a23ee56368d3de95e8e2cce6a1e5
matthew-sullivan <contact@matthewsullivan.media>
2019-05-26 20:32:44 -0400

    Add places controller

---
+++ modules/places/controller/places.controller.js
@@ -0,0 +1,17 @@
+module.exports = {
+  /**
+   * Get Places
+   * @async
+   * @param {object} ctx
+   */
+  getPlaces: async (ctx) => {
+    // const exampleId = ctx.params.id;
+    // const result = await exampleService.getExampleById(exampleId);
+
+    // const example = result.rows[0];
+
+    // ctx.body = example;
+
+    ctx.body = 'Hello, World!';
+  },
+};
