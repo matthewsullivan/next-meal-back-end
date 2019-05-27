0ac3d02f88937758c54ce677ccdae3812d22be07
matthew-sullivan <contact@matthewsullivan.media>
2019-05-26 20:35:29 -0400

    Remove acl from places routes

--- modules/places/routes/places.routes.js
+++ modules/places/routes/places.routes.js
@@ -1,23 +1,10 @@
-const Acl = require('acl');
 const router = require('koa-router')();
 const path = require('path');
-
-const acl = new Acl(new Acl.memoryBackend());
 
 const placesController = require(path.resolve(
   './modules/places/controllers/places.controller.js'
 ));
 
-acl.allow([
-  {
-    allows: [
-      {
-        permissions: '*',
-        resources: '/api/v2/places',
-      },
-    ],
-  },
-]);
 
 router
   .get('/api/v2/places', secured, placesController.getPlaces)
