b776a5991298f2199c1fefd59846e2361bffa48a
matthew-sullivan <contact@matthewsullivan.media>
2019-05-26 20:15:49 -0400

    Update server

--- server.js
+++ server.js
@@ -1,19 +1,8 @@
-const compression = require('compression');
 const express = require('express');
-const prpl = require('prpl-server');
-
 const app = express();
-const config = require('./build/polymer.json');
 
-app.set('trust proxy', true);
-
-app.use(compression());
-app.use(express.static('static', {dotfiles: 'allow'}));
-
-app.get('/*', prpl.makeHandler('./build/', config));
-
-app.listen(80, () => {
-  console.log(`\nNext Meal`);
-  console.log(`Path: 127.0.0.1`);
+app.listen(3000, () => {
+  console.log(`\nNext Meal Back End`);
+  console.log(`Path: 127.0.0.1:3000`);
   console.log(`${new Date().toString()}\n`);
 });
