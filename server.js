ca94c5baa0952aa91d5d4cd980c1d365d43a286c
matthew-sullivan <contact@matthewsullivan.media>
2019-06-10 18:47:30 -0400

    Implement place details controller

--- server.js
+++ server.js
@@ -79,13 +79,3 @@ console.log(`Path: 127.0.0.1:3000`);
 console.log(`${new Date().toString()}\n`);
 
 module.exports = app;
-
-
-// const express = require('express');
-// const app = express();
-
-// app.listen(3000, () => {
-//   console.log(`\nNext Meal Back End`);
-//   console.log(`Path: 127.0.0.1:3000`);
-//   console.log(`${new Date().toString()}\n`);
-// });
